90cca4fe4f684363862af85ea0717910c561f288
marionl4b <marion.labarre@gmail.com>
2018-05-14 14:42:32 +0200

    Add basic grid setup

--- program.py
+++ program.py
@@ -1,2 +1,25 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+import random
+
+##################################################
+#################### GRID SETUP ##################
+##################################################
+
+wall = "X"
+road = " "
+macGyver = "M"
+gateKeeper = "G"
+objects = ["1", "2", "3"]
+
+def grid():
+    grid = " "
+    for line in range(15):
+        line = [wall] * 7 + [road] * 8
+        random.shuffle(line)
+        line = " ".join(line)+"\n"
+        grid += line
+    return grid
+
+print(grid())
