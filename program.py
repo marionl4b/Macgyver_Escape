770f90a09f8a4b9b278209b336261ddd79732801
marionl4b <marion.labarre@gmail.com>
2018-05-24 10:58:24 +0200

    Add player move loop fonction after grid init correction

--- program.py
+++ program.py
@@ -3,8 +3,6 @@
 
 import random
 import os
-import pickle
-import itertools
 
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))  # return api directory
 WALL = "X"
@@ -12,68 +10,74 @@ ROAD = " "
 MACGYVER = "M"
 GATE_KEEPER = "G"
 OBJECTS = ["1", "2", "3"]
+DIRECTIONS = ("z", "q", "s", "d")
 
 
-def store_grid():
+class Labyrinth:
+    def __init__(self):
+        self.grid = []
+        self.mg_pos = 0
+        self.gk_pos = 250
 
-    """ setup and store random labyrinth grid 15x15 in grid.txt """
+        self.init_grid()
+        self.load_grid()
+        self.show_grid()
+        self.player_move()
 
-    stored_grid = []
-    i = 0
-    for line in range(15):
-        i += 1
-        line = [WALL] * 5 + [ROAD] * 10
-        while i == 1:
-            line[0] = MACGYVER
-            break
-        random.shuffle(line)
-        # line = " ".join(line)+"\n"
-        stored_grid += [line]
-    with open(os.path.join(DIR_PATH, 'grid.txt'), 'wb') as file:
-        mon_pickler = pickle.Pickler(file)
-        mon_pickler.dump(stored_grid)
-    # return stored_grid
+    def init_grid(self):
 
+        """ setup and store random labyrinth grid 15x15 in grid.txt """
+        init_grid = str()
+        for line in range(15):
+            line = [WALL] * 5 + [ROAD] * 10
+            random.shuffle(line)
+            line = " ".join(line)+"\n"
+            init_grid += line
+        with open(os.path.join(DIR_PATH, 'grid.txt'), 'w') as file:
+            file.write(init_grid)
 
-def show_grid():
+    def load_grid(self):
 
-    """ show the grid stored before """
+        with open(os.path.join(DIR_PATH, 'grid.txt'), 'r') as file:
+            self.grid = list(file.read())
+            self.grid[self.mg_pos] = MACGYVER
+            self.grid[self.gk_pos] = GATE_KEEPER
+        return self.grid
 
-    with open(os.path.join(DIR_PATH, 'grid.txt'), 'rb') as file:
-        mon_depickler = pickle.Unpickler(file)
-        show_grid = mon_depickler.load()
-    # show_grid = " ".join(show_grid(listes)+"\n"
-    return(show_grid)
+    def show_grid(self):
 
-def player_move():
+        """ show the grid loaded before """
+        for line in self.grid:
+            print(line, end='')
+        # print(self.grid)
 
-    """take keyboard action and make MacGyver move on the grid in theory"""
-    # new_grid = stored_grid
-    key_board = input()
-    if key_board == "u":
-        #MACGYVER indice -1 move left
-        pass
-    elif key_board == "i":
-        #MACGYVER indice +1 move right
-        pass
-    elif key_board == "j":
-        #MACGYVER indice -15 move up
-        pass
-    elif key_board == "k":
-        #MACGYVER indice +15 move down
-        pass
-    else:
-        #MACGYVER initial indice stay in place
-        pass
-    #return new grid for stored_grid()
+    def player_move(self):
+
+        """take keyboard action and make MacGyver move on the grid"""
+
+        while True:
+            direction = input("Déplacez MacGyver dans le Labyrinthe grace aux touches ZQSD:")
+            if direction in DIRECTIONS:
+                if direction == "z":
+                    self.mg_pos -= 30  #move up
+                elif direction == "q":
+                    self.mg_pos -= 2  #move left
+                elif direction == "s":
+                    self.mg_pos += 30  #move right
+                elif direction == "d":
+                    self.mg_pos += 2  #move down
+            else:
+                pass
+            #
+            self.load_grid()
+            self.show_grid()
 
 
 def main():
 
     """ Merlin : what's you're favorite colour ? Galaad : Blue, no Red, aaaaargh """
 
-    store_grid()
-    print(show_grid())
+    Labyrinth()
 
 
 if __name__ == "__main__":
