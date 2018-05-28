8fa83662a82f77362d54be97d8b32ae812190842
marionl4b <marion.labarre@gmail.com>
2018-05-28 11:21:16 +0200

    Restrict MacGyver to move inside the grid

--- program.py
+++ program.py
@@ -16,6 +16,10 @@ DIRECTIONS = ("z", "q", "s", "d")
 class Labyrinth:
     def __init__(self):
         self.grid = []
+        self.grid_up_pos = []
+        self.grid_down_pos = []
+        self.grid_left_pos = []
+        self.grid_right_pos = []
         self.mg_pos = 0
         self.gk_pos = 250
 
@@ -23,6 +27,7 @@ class Labyrinth:
         self.load_grid()
         self.show_grid()
         self.player_move()
+
 
     def init_grid(self):
 
@@ -40,8 +45,17 @@ class Labyrinth:
 
         with open(os.path.join(DIR_PATH, 'grid.txt'), 'r') as file:
             self.grid = list(file.read())
+            self.grid_up_pos = [i for i in range(0, 28, 2)]
+            self.grid_down_pos = [i for i in range(420, len(self.grid), 2)]
+            self.grid_left_pos = [i for i in range(0, len(self.grid), 30)]
+            self.grid_right_pos = [i for i in range(28, len(self.grid), 30)]
             self.grid[self.mg_pos] = MACGYVER
             self.grid[self.gk_pos] = GATE_KEEPER
+
+        return self.grid_up_pos
+        return self.grid_down_pos
+        return self.grid_left_pos
+        return self.grid_right_pos
         return self.grid
 
     def show_grid(self):
@@ -54,25 +68,28 @@ class Labyrinth:
 
         """take keyboard action and make MacGyver move on the grid"""
 
-        while True:
+        while True:  # while player move
             direction = input("Déplacez MacGyver dans le Labyrinthe grace aux touches ZQSD:")
 
-            if direction in DIRECTIONS:
-                if direction == "z" and self.grid[self.mg_pos - 30] != WALL:
+            if direction in DIRECTIONS:  # test input
+                if direction == "z" and self.grid[self.mg_pos - 30] != WALL and self.mg_pos not in self.grid_up_pos:
                     self.mg_pos -= 30   # move up
-                elif direction == "q" and self.grid[self.mg_pos - 2] != WALL:
+                elif direction == "q" and self.grid[self.mg_pos - 2] != WALL and self.mg_pos not in self.grid_left_pos:
                     self.mg_pos -= 2    # move left
-                elif direction == "s" and self.grid[self.mg_pos + 30] != WALL:
+                elif direction == "s" and self.grid[self.mg_pos + 30] != WALL \
+                        and self.mg_pos not in self.grid_down_pos_pos:
                     self.mg_pos += 30   # move right
-                elif direction == "d" and self.grid[self.mg_pos + 2] != WALL:
+                elif direction == "d" and self.grid[self.mg_pos + 2] != WALL and self.mg_pos not in self.grid_right_pos:
                     self.mg_pos += 2    # move down
             if self.mg_pos == self.gk_pos:
                 print("Gagné!")
+                victory = True
                 break
             else:
                 pass
             self.load_grid()
             self.show_grid()
+            print(self.mg_pos)
 
 
 def main():
